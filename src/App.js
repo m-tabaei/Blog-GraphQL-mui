ee36c9378eafadfd541a55699db4edee38694149
mtabaie <mtabaie@gmail.com>
2023-11-13 16:26:57 +0330

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Home from "./Component/Home/Home";
 import Layouts from "./Component/layout/Layouts";
 
 
 function App() {
   return (
     <Layouts>
-     
+     <Home/>
     </Layouts>
   );
 }
--- src/Component/Blog/Blogs.js
+++ src/Component/Blog/Blogs.js
@@ -1,9 +1,17 @@
+import { useQuery } from "@apollo/client";
+import { GET_BLOGS_INFO } from "../../GraphQL/queries";
+
 const Blogs = () => {
-    return ( 
-        <>
-        <h1>Blogs</h1>
-        </>
-     );
-}
- 
+  const { loading, data, error } = useQuery(GET_BLOGS_INFO);
+  if (loading) return <h4>صبور باشید ... </h4>;
+
+  if (error) return <h4>Error ...</h4>;
+  console.log(data);
+  return (
+    <>
+      <h1>Blogs</h1>
+    </>
+  );
+};
+
 export default Blogs;
